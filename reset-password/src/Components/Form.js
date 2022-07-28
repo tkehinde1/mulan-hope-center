74e985023b01ab835d7afab996181dbd15d07e47
GonzalezJaziel <108748196+GonzalezJaziel@users.noreply.github.com>
2022-07-28 09:15:02 -0400

    Create Form.js

---
+++ reset-password/src/Components/Form.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import SignUpInfo from "./SignUpInfo";
+import EnterInfo from "./EnterInfo";
+import ForgotPasswordVerify from "./ForgotPasswordVerify";
+import Address from "./Address";
+import DeleteAccount from "./DeleteAccount";
+
+function Form() {
+  const [page, setPage] = useState(0);
+  const [formData, setFormData] = useState({
+
+  });
+
+  const FormTitles = ["Address", "", "", "", "" ];
+
+  const PageDisplay = () => {
+    if (page === 0) {
+      return <SignUpInfo formData={formData} setFormData={setFormData} />;
+    } else if (page === 1) {
+      return <EnterInfo formData={formData} setFormData={setFormData} />;
+    } else if (page === 2){
+      return <ForgotPasswordVerify formData={formData} setFormData={setFormData} />;
+    } else if (page === 3){
+      return <Address formData={formData} setFormData={setFormData} />;
+    }else if (page === 4) {
+      return <DeleteAccount formData={formData} setFormData={setFormData} />;
+    }
+  };
+
+  return (
+    <div className="form">
+      <div className="progressbar">
+        <div
+          style={{ width: page === 0 ? "33.3%" : page == 1 ? "66.6%" : "100%" }}
+        ></div>
+      </div>
+      <div className="form-container">
+        <div className="header">
+          <h1>{FormTitles[page]}</h1>
+        </div>
+        <div className="body">{PageDisplay()}</div>
+        <div className="footer">
+          <button
+            disabled={page == 0}
+            onClick={() => {
+              setPage((currPage) => currPage - 1);
+            }}
+          >
+            Prev
+          </button>
+          <button
+            onClick={() => {
+              if (page === FormTitles.length - 1) {
+                alert("FORM SUBMITTED");
+                console.log(formData);
+              } else {
+                setPage((currPage) => currPage + 1);
+              }
+            }}
+          >
+            {page === FormTitles.length - 1 ? "Submit" : "Next"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Form;
