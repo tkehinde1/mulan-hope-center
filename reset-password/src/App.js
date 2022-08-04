65b29b4baab5d3405382798c725cacff8aba8580
GonzalezJaziel <108748196+GonzalezJaziel@users.noreply.github.com>
2022-08-04 08:19:59 -0400

    Update App.js

--- reset-password/src/App.js
+++ reset-password/src/App.js
@@ -1,23 +1,40 @@
-import * as React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Paper from '@mui/material/Paper';
-import { Box, Typography } from '@mui/material';
-import Button from '@mui/material/Button';
-import EnterInfo from './Components/EnterInfo.js';
-import EnterCode from './Components/EnterCode.js';
-import NewPassword from './Components/NewPassword.js';
-
-
+import './App.css'
+import {BrowserRouter as Router, Routes, Route}from 'react-router-dom'
+import NewPassword from "./Components/NewPassword";
+import Login from "./Components/Login"
+import SignUp from "./Components/SignUp"
+import TwoFactor from "./Components/TwoFactor"
+import TwoFactorTwo from "./Components/TwoFactorTwo"
+import Address from "./Components/Address";
+import DeleteAccount from "./Components/DeleteAccount";
+import DonationPage from "./Components/DonationPage";
+import EnterCode from "./Components/EnterCode";
+import EnterInfo from "./Components/EnterInfo";
+import HomePage from "./Components/HomePage";
+import AccountPage from './Components/AccountPage';
 function App() {
   return (
-     //<EnterInfo/>
-      //<EnterCode/>
-      <NewPassword/>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Donation" element={<DonationPage />} />
+        <Route path="/Signup" element={<SignUp />} />
+        <Route path="/Home" element={<HomePage />} />
+        <Route path="/2FA" element={<TwoFactor />} />
+        <Route path="/2FA2" element={<TwoFactorTwo />} />
+        <Route path="/EnterInfo" element={<EnterInfo />} />
+        <Route path="/EnterCode" element={<EnterCode />} />
+        <Route path="/NewPassword" element={<NewPassword />} />
+        <Route path="/HopeCenterAddress" element={<Address />} />
+        <Route path="/DeleteAccount" element={<DeleteAccount />} />
+        <Route path="/AccountPage" element={<AccountPage />} />
+      </Routes>
+     </Router>
+      );
 }
 
 export default App;
+
 
 
 
