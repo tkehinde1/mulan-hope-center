fd7a93ad53341a1b758b78c8fd05bb397b2fe2f7
GunturiVishnu <108748270+GunturiVishnu@users.noreply.github.com>
2022-08-11 11:50:58 -0400

    Update AccountPage.js

--- reset-password/src/Components/AccountPage.js
+++ reset-password/src/Components/AccountPage.js
@@ -1,38 +1,31 @@
 import {
-  AccountBox,
-  Article,
-  Group,
-  Home,
-  ModeNight,
-  Person,
-  Settings,
-  Storefront,
-} from "@mui/icons-material";
-import {
   Typography,
   Button,
   Box,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Switch,
-  Paper,
 } from "@mui/material";
 import React from "react";
 import ButtonTheme from './ButtonTheme'
 import { ThemeProvider } from '@mui/material';
 import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
-import { useNavigate } from 'react-router-dom';
-
+import hopeCenterLogo from '/workspace/mulan-hope-center/reset-password/src/Components/Hope-Center-Logo.png';
+const boxstyle2 = {
+  color: "white",
+  background: "#3449B5",
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  position: 'right',
+  flexWrap: 'wrap',
+  height: '3vh',
+  width: '206vh'
+}
 const boxstyle1 = {
   color: "white",
   background: "#3449B5",
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'flex-start',
-  position: 'center',
+  position: 'left',
   flexWrap: 'wrap',
   height: '5vh',
   width: '206vh'
@@ -44,49 +37,63 @@ const boxstyle = {
   alignItems: 'center',
   justifyContent: 'space-between',
   position: 'center',
-  flexWrap: 'wrap',
+  
   height: '95vh',
   width: '206vh',
   display: 'flex',
-  flexDirection: 'column',
+  flexDirection: 'column', 
+ 
 }
-function Account ({mode,setMode}) {
-  let navigate = useNavigate();
 
+
+
+const Account = ({mode,setMode}) => {
   return (
+   
     <ThemeProvider  theme={ButtonTheme}>
-      <Box   sx={{display:'flex', alignItems: 'center', }} style={boxstyle1}>
+      <Box sx={{width: '200'}}>
+     
+      <Box   sx={{display:'flex', alignItems: 'flex-start', }} style={boxstyle1}>
     <ArrowLeftIcon sx={{ fontSize: 40 }}/>
-  <Button onClick={() => {navigate("/SettingsP")}} justifyContent="flex-start" style={{ maxWidth: "100px",minWidth: "100px", maxHeight: "30px",minHeight: "30px"}} type="submit" size="large" variant="contained">
+  <Button justifyContent="flex-start" style={{ maxWidth: "100px",minWidth: "100px", maxHeight: "30px",minHeight: "30px"}} type="submit" size="large" variant="contained">
   Back
 </Button>
   </Box>
+  <Box   sx={{display:'flex', alignItems: 'flex-end', }} style={boxstyle2}>
+<img justifyContent="flex-end" src={hopeCenterLogo} height="75" width="150" alt="hopeCenterlogo"/>
+  </Box>
+
     <Box   sx={{display:'flex', alignItems: 'center', }} style={boxstyle}>
       <Typography color ="yellow" variant="h1" component="div" gutterBottom>
         Account
       </Typography>
-<Button onClick={() => {navigate("/NewPAssword")}} style={{ maxWidth: "500px",minWidth: "500px"}} type="submit" size="large" variant="contained">
+<Button style={{ maxWidth: "500px",minWidth: "500px"}} type="submit" size="large" variant="contained">
   Change Password
 </Button>
+    
 <Button style={{ maxWidth: "500px",minWidth: "500px"}} size="large" variant="contained">
   Donation History
 </Button>
-<Button onClick={() => {navigate("/2FA")}} style={{ maxWidth: "500px",minWidth: "500px"}} size="large" variant="contained">
+<Button style={{ maxWidth: "500px",minWidth: "500px"}} size="large" variant="contained">
   2 Factor Authentication
 </Button>
-<Button onClick={() => {navigate("/")}} style={{ maxWidth: "500px",minWidth: "500px"}} size="large" variant="contained">
+<Button style={{ maxWidth: "500px",minWidth: "500px"}} size="large" variant="contained">
   Logout
 </Button>
 <Button style={{ maxWidth: "500px",minWidth: "500px"}} size="large" variant="contained">
   Help
 </Button>
-<Button onClick={() => {navigate("/DeleteAccount")}} style={{ maxWidth: "500px",minWidth: "500px"}} size="large" variant="contained">
+<Button style={{ maxWidth: "500px",minWidth: "500px"}} size="large" variant="contained">
   Delete Account
 </Button>
+
+
+</Box>
 </Box>
 </ThemeProvider>
   );
 };
+
 export default Account;
 
 
